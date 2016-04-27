964dc019b1cdf09e04eebc8cc73042bcc670715e
FilipinoKidz <warrene@siu.edu>
2016-04-27 15:54:18 -0500

    Added to User.h and created User.cpp

--- ConsoleApplication1/User.h
+++ ConsoleApplication1/User.h
@@ -1,6 +1,23 @@
 #include<string>
+#include "Transaction.h"
 using namespace std;
 class User{
-	string name;
-	
+	string fname;
+	string lname;
+	string username;
+	string password;
+	string card_num;
+	Transaction *list;
+public:
+	User();
+	void set_fname();
+	void set_lname();
+	void set_username();
+	void set_password();
+	void assign_card_num();
+	Transaction *addto(Transaction *);
+	Transaction *deletefrom(Transaction *);
+	double average(Transaction *);
+	double standard_dv(Transaction *);
+	bool dev_check(Transaction, Transaction *)
 };
