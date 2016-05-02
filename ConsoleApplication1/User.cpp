a894e101b0dbbacf0b50b1896a73e33f56baa107
FilipinoKidz <warrene@siu.edu>
2016-05-02 15:56:58 -0500

    Worked on User.cpp

--- ConsoleApplication1/User.cpp
+++ ConsoleApplication1/User.cpp
@@ -1,1 +1,22 @@
 #include "User.h"
+#include <iostream>
+
+User::User(string fn, string ln, string un, string pw){
+	fname = fn;
+	lname = ln;
+	username = un;
+	password = pw;
+	card_num = "Not yet assigned";
+	pin = 0;
+}
+
+User User::new_user(){
+	string fn;
+	string ln;
+	string un;
+	string pw;
+	cout << "First Name: ";
+	cin >> fn;
+	cout << "Last Name: ";
+	cin >> ln;
+}
--- ConsoleApplication1/User.h
+++ ConsoleApplication1/User.h
@@ -5,20 +5,30 @@ using namespace std;
 class User{
 	string fname;
 	string lname;
+	int age;
+	// address
 	string username;
 	string password;
 	string card_num;
+	int pin;
 	List *list;
 public:
-	User();
+	User(string fn, string ln, string un, string pw);
+	User new_user();
+	string get_fname();
+	string get_lname();
+	string get_username();
+	string get_password();
+	int get_pin();
 	void set_fname();
 	void set_lname();
 	void set_username();
 	void set_password();
+	void set_pin();
 	void assign_card_num();
 	List *addto(List *);
 	List *deletefrom(List *);
-	double average(List *);
-	double standard_dv(List *);
+	Transaction average(List *);
+	Transaction standard_dv(List *);
 	bool dev_check(Transaction, List *);
 };
